e171960f693f68ea5f1d2dcda52ab0703fadaff5
Chris Havel <christopher.havel@me.com>
2020-12-10 18:11:40 -0500

    another init

--- js/init.js
+++ js/init.js
@@ -5,3 +5,30 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function onEnter(){
+  var button = document.getElementById('button');
+  button.addEventListener('click',save,false);
+  display();
+}
+
+function display(){
+  var display-data = document.getElementById('display-data');
+  display-data-innerHTML="";
+  for(var i = 0; i < sessionStorage.length; i++){
+    var a = sessionStorage.key(i);
+    var b = sessionStorage.getItem(a);
+    display-data.innerHTML += a;
+  }
+}
+
+function save(){
+  var email = document.getElementById('email');
+  var emailName = document.getElementById('emailName');
+  sessionStorage.setItem(email);
+  display();
+  key.value="";
+  value.value="";
+}
+
+window.addEventListener("load",onEnter,false);
